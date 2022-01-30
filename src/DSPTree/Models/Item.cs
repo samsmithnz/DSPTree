3d57587921e0caa8ddd1472c5b1099939ce9d064
Sam Smith <samsmithnz@gmail.com>
2022-01-30 12:24:21 -0500

    Initial project creation

---
+++ src/DSPTree/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace DSPTree.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public Dictionary<string,int> Recipe { get; set; }
+        public string ManufactoringMethod { get; set; }
+
+    }
+}
