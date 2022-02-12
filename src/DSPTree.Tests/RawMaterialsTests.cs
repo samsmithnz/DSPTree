cf501ce0413504cee1adda6e555ddb3c70a409b0
Sam Smith <samsmithnz@gmail.com>
2022-02-12 08:49:47 -0500

    Added new tests for raw materials

--- src/DSPTree.Tests/RawMaterialsTests.cs
+++ src/DSPTree.Tests/RawMaterialsTests.cs
@@ -48,4 +48,34 @@ public class RawMaterialsTests
         Assert.IsTrue(rawMaterials.ContainsKey("Iron Ore"));
         Assert.AreEqual(2, rawMaterials["Iron Ore"]);
     }
+
+    [TestMethod]
+    public void QuantumChipRawMaterialsTest()
+    {
+        //Arrange
+        DSPGraph graph = new();
+        Item startingItem = ItemPoolLevel8.QuantumChip();
+
+        //Act
+        Dictionary<string, int> rawMaterials = RawMaterials.CalculateRawMaterials(graph.Items, startingItem);
+
+        //Assert
+        Assert.AreEqual(8, rawMaterials.Count);
+        Assert.IsTrue(rawMaterials.ContainsKey("Copper Ore"));
+        Assert.AreEqual(8, rawMaterials["Copper Ore"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Iron Ore"));
+        Assert.AreEqual(4, rawMaterials["Iron Ore"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Silicon Ore"));
+        Assert.AreEqual(16, rawMaterials["Silicon Ore"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Titanium Ore"));
+        Assert.AreEqual(28, rawMaterials["Titanium Ore"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Crude Oil"));
+        Assert.AreEqual(66, rawMaterials["Crude Oil"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Stone"));
+        Assert.AreEqual(24, rawMaterials["Stone"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Water"));
+        Assert.AreEqual(14, rawMaterials["Water"]);
+        Assert.IsTrue(rawMaterials.ContainsKey("Coal"));
+        Assert.AreEqual(16, rawMaterials["Coal"]);
+    }
 }
