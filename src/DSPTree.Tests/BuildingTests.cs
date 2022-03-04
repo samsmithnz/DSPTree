78ad9d358e673c3de882e8a4124c3e0e8285b4d0
Sam Smith <samsmithnz@gmail.com>
2022-03-04 09:27:48 -0500

    Added  debug building output

--- src/DSPTree.Tests/BuildingTests.cs
+++ src/DSPTree.Tests/BuildingTests.cs
@@ -1,6 +1,9 @@
 using DSPTree.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace DSPTree.Tests;
 
@@ -42,5 +45,43 @@ public class BuildingTests
         Assert.AreEqual("Icon_Solar_Panel.png", solarPanel?.Image);
         Assert.AreEqual(4, solarPanel?.Level);
     }
+
+    [TestMethod]
+    public void GetBuildingMaterialsTest()
+    {
+        //Arrange
+        DSPGraph graph = new("", ResearchType.WhiteScience, true);
+
+        //Act
+        List<string> items = new();
+        foreach (Item? item in graph.Items)
+        {
+            if (item.ItemType == ItemType.Building)
+            {
+                foreach (Recipe recipe in item.Recipes)
+                {
+                    StringBuilder sb = new();
+                    sb.Append(item.Name);
+                    foreach (System.Collections.Generic.KeyValuePair<string, int> input in recipe.Inputs)
+                    {
+                        sb.Append(input.Key);
+                        sb.Append(',');
+                    }
+                    items.Add(sb.ToString());
+                }
+            }
+        }
+
+        //Assert
+        Assert.IsNotNull(items);
+        Assert.IsTrue(items.Count > 0);
+        foreach (string item in items)
+        {
+            Debug.WriteLine(item);
+        }
+
+    }
+
+
 
 }
