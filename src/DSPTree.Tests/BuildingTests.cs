2efcbdbd432da6ed02cde8a5343ffe4871bfd941
Sam Smith <samsmithnz@gmail.com>
2022-12-27 08:15:38 -0500

    Fixed other tests

--- src/DSPTree.Tests/BuildingTests.cs
+++ src/DSPTree.Tests/BuildingTests.cs
@@ -64,7 +64,7 @@ public class BuildingTests
 
         //Assert
         Assert.IsNotNull(graph);
-        Assert.AreEqual(48, buildingCount);
+        Assert.AreEqual(51, buildingCount);
     }
 
     [TestMethod]
@@ -113,7 +113,7 @@ public class BuildingTests
 
         //Assert
         Assert.IsNotNull(graph.Items);
-        Assert.AreEqual(82, graph.Items.Count);
+        Assert.AreEqual(87, graph.Items.Count);
         foreach (Item? item in graph.Items)
         {
             if (item.Name == "Accumulator")
