f5e39b6f288f317c39e7771e44320c7f9b9369de
Sam Smith <samsmithnz@gmail.com>
2022-12-27 08:21:59 -0500

    FIxed tests

--- src/DSPTree.Tests/D3GraphTests.cs
+++ src/DSPTree.Tests/D3GraphTests.cs
@@ -22,6 +22,9 @@ public class D3GraphTests
         Assert.IsNotNull(graph);
         Assert.IsTrue(graph.nodes.Count > 0);
         Assert.IsTrue(graph.links.Count > 0);
+        Assert.IsTrue(graph.links[0].source >= 0);
+        Assert.IsTrue(graph.links[0].target >= 0);
+        Assert.IsTrue(graph.links[0].value >= 0);
         //Assert.AreEqual("Iron Ore Vein", graph.Items[0].Name);
         //Assert.AreEqual("80px-Icon_Iron_Ore_Vein.png", graph.Items[0].Image);
         //Assert.AreEqual(0, graph.Items[0].Level);
